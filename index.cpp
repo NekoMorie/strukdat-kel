872c6b6acfca30972f1f29db11e87a1a2ebb9bf4
NekoMorie <79540250+NekoMorie@users.noreply.github.com>
2023-10-24 14:20:46 +0700

    UTS

--- index.cpp
+++ index.cpp
@@ -1,203 +1,154 @@
 #include <iostream>
+
 using namespace std;
-/* struct : data yang dibentuk oleh beberapa data */
-struct buah
+
+struct mahasiswa
 {
     string nama;
-    string warna;
-    float berat;
-    int harga;
-    string rasa;
-    // pointer untuk menghubungkan node
-    buah *next;
+    int NIM;
+    float IPK;
+
+    mahasiswa *next;
 };
-buah *head, *tail, *nodeSekarang, *nodeBaru, *temporaryNode;
-/*
- * Prototipe Fungsi
- */
-void buatSingleLinkedList(string, string, float, int, string);
-void masukanNodeKeAwal(string, string, float, int, string);
-void masukanNodeKeAkhir(string, string, float, int, string);
-void rubahDataNodePertama(string, string, float, int, string);
-void rubahDataNodeTerakhir(string, string, float, int, string);
-void hapusNodePertama();
-void hapusNodeTerakhir();
-void cetakSingleLinkedList();
-void cetakSeluruhAtributNode();
-/*
- * Fungsi Utama
- */
+
+mahasiswa *head, *tail, *nodesekarang, *nodebaru, *temp;
+
+void tambah()
+{
+    nodebaru = new mahasiswa;
+    cout << "Masukkan Nama : ";
+    cin >> nodebaru->nama;
+    cout << "Masukkan NIM : ";
+    cin >> nodebaru->NIM;
+    cout << "Masukkan IPK : ";
+    cin >> nodebaru->IPK;
+    nodebaru->next = NULL;
+    if (head == NULL)
+    {
+        head = nodebaru;
+    }
+    else
+    {
+        mahasiswa *p = head;
+        while (p->next != NULL)
+        {
+            p = p->next;
+        }
+        p->next = nodebaru;
+    }
+}
+
+void tampil()
+{
+    mahasiswa *p;
+    p = head;
+    while (p != NULL)
+    {
+        cout << "nama : " << p->nama << "\t NIM : " << p->NIM << "\t IPK : " << p->IPK << endl;
+        p = p->next;
+    }
+}
+
+void urut()
+{
+    mahasiswa *p, *q, *r;
+    p = head;
+    while (p != NULL)
+    {
+        q = p->next;
+        while (q != NULL)
+        {
+            if (p->IPK > q->IPK)
+            {
+                r = q;
+                q = q->next;
+                r->next = p;
+                p = r;
+            }
+            else
+            {
+                break;
+            }
+        }
+        p = q;
+    }
+}
+
 int main()
 {
-    /* membuat node pertama kali */
-    cout << endl
-         << "01. Membuat Node Pertama:" << endl;
-    buatSingleLinkedList("Mangga", "Jingga", 1.1, 10000, "Manis");
-    cetakSingleLinkedList(); // menampilkan node
-    /* menambahkan node di awal*/
-    cout << endl
-         << "02. Menambahkan node pada posisi awal:" << endl;
-    masukanNodeKeAwal("Pisang", "Kuning", 1.3, 8000, "Manis");
-    cetakSingleLinkedList(); // menampilkan node
-    /* menambahkan node di akhir*/
-    cout << endl
-         << "03. Menambahkan node pada posisi akhir:" << endl;
-    masukanNodeKeAkhir("Apel", "Merah", 2.1, 13000, "Manis");
-    cetakSingleLinkedList(); // menampilkan node
-    /* menambahkan node di awal*/
-    cout << endl
-         << "04. Menambahkan node pada posisi awal lagi:" << endl;
-    masukanNodeKeAwal("Anggur", "Ungu", 1, 9000, "Manis");
-    cetakSingleLinkedList(); // menampilkan node
-    /* menghapus node di awal*/
-    cout << endl
-         << "05. menghapus node pada posisi awal:" << endl;
-    hapusNodePertama();
-    cetakSingleLinkedList();
-    /* menghapus node di akhir*/
-    cout << endl
-         << "06. menghapus node pada posisi akhir:" << endl;
-    hapusNodeTerakhir();
-    cetakSingleLinkedList();
-    /* menghapus node di akhir*/
-    cout << endl
-         << "07. menghapus node pada posisi akhir:" << endl;
-    hapusNodeTerakhir();
-    cetakSingleLinkedList();
-    return 0;
-} // akhir int main
-/*
- * Fungsi
- */
-// fungsi membuat node pertama
-void buatSingleLinkedList(string nama, string warna, float berat, int harga, string rasa)
-{
-    // membuat node pertama kali
-    head = new buah();
-    head->nama = nama;
-    head->warna = warna;
-    head->berat = berat;
-    head->harga = harga;
-    head->rasa = rasa;
-    // maka node tersebut akan jadi head dan tail
-    head->next = NULL;
-    tail = head;
-}
-// fungsi membuat node di awal
-void masukanNodeKeAwal(string nama, string warna, float berat, int harga, string rasa)
-{
-    // buat node baru
-    nodeBaru = new buah();
-    nodeBaru->nama = nama;
-    nodeBaru->warna = warna;
-    nodeBaru->berat = berat;
-    nodeBaru->harga = harga;
-    nodeBaru->rasa = rasa;
-    // node baru di awal menunjuk ke head
-    nodeBaru->next = head;
-    // tukar head menjadi yang node baru
-    // maka sekarang head adalah node baru
-    head = nodeBaru;
-}
-// fungsi membuat node di akhir
-void masukanNodeKeAkhir(string nama, string warna, float berat, int harga, string rasa)
-{
-    // membuat node baru
-    nodeBaru = new buah();
-    nodeBaru->nama = nama;
-    nodeBaru->warna = warna;
-    nodeBaru->berat = berat;
-    nodeBaru->harga = harga;
-    nodeBaru->rasa = rasa;
-    // node di akhir harus menunjuk null
-    nodeBaru->next = NULL;
-    // node yang sebelumnya tail harus ditunjuk ke node baru
-    tail->next = nodeBaru;
-    // maka sekarang tail adalah node baru
-    tail = nodeBaru;
-}
-// fungsi rubah data node pertama
-void rubahDataNodePertama(string nama, string warna, float berat, int harga, string rasa)
-{
-    head->nama = nama;
-    head->warna = warna;
-    head->berat = berat;
-    head->harga = harga;
-    head->rasa = rasa;
-}
-// fungsi rubah data node terakhir
-void rubahDataNodeTerakhir(string nama, string warna, float berat, int harga, string rasa)
-{
-    // ubah data node terakhir
-    tail->nama = nama;
-    tail->warna = warna;
-    tail->berat = berat;
-    tail->harga = harga;
-    tail->rasa = rasa;
-}
-// fungsi hapus node pertama
-void hapusNodePertama()
-{
-    // simpan node head ke node sementara
-    temporaryNode = head;
-    // node head diganti dengan node selanjutnya
-    head = head->next;
-    delete temporaryNode;
-}
-// fungsi hapus node terakhir
-void hapusNodeTerakhir()
-{
-    // simpan node tail ke node sementara
-    temporaryNode = tail;
-    // mencari nodetail
-    // pencarian dimulai dari head
-    nodeSekarang = head;
-    while (nodeSekarang->next != NULL)
+    int pilih;
+    cout << "=========================Menu========================" << endl;
+    cout << "1. Tambah Mahasiswa" << endl;
+    cout << "2. Tampilkan Mahasiswa" << endl;
+    cout << "3. Urutkan Mahasiswa Berdasarkan NIM" << endl;
+    cout << "4. Keluar" << endl;
+    cout << "================================================================\n";
+    cout << "Pilih menu :";
+    cin >> pilih;
+    if (pilih == 1)
     {
-        // jika node sekarang itu next nodenya adalah tail
-        // eksekusi
-        if (nodeSekarang->next == tail)
-        {
-            // node sekarang jadi tail
-            // dengan membuah next dari node sekarang menjadi null bukan ke node lain
-            nodeSekarang->next = NULL;
-            // maka tail dirubah node sekarang
-            tail = nodeSekarang;
-        }
-        else
-        {
-            // jika tidak maka current Node diganti dengan node selanjutnya
-            nodeSekarang = nodeSekarang->next;
-        }
+
+        tambah();
+        system("pause");
+        system("cls");
+        return main();
     }
-    delete temporaryNode;
-}
-// fungsi cetak linked list
-void cetakSingleLinkedList()
-{
-    // dimulai dari node head
-    nodeSekarang = head;
-    while (nodeSekarang != NULL)
+    else if (pilih == 2)
     {
-        cout << nodeSekarang->nama << " >> ";
-        // geser ke node selanjutnya
-        nodeSekarang = nodeSekarang->next;
+        tampil();
+        system("pause");
+        system("cls");
+        return main();
     }
-    cout << "!!n";
-}
-// fungsi cetak seluruh data node linked list
-void cetakSeluruhAtributNode()
-{
-    // dimulai dari node head
-    nodeSekarang = head;
-    while (nodeSekarang != NULL)
+    else if (pilih == 3)
     {
-        cout << "nama: " << nodeSekarang->nama << endl;
-        cout << "warna: " << nodeSekarang->warna << endl;
-        cout << "berat: " << nodeSekarang->berat << endl;
-        cout << "harga: " << nodeSekarang->harga << endl;
-        cout << "rasa: " << nodeSekarang->rasa << endl;
-        cout << endl;
-        nodeSekarang = nodeSekarang->next;
+        urut();
+        system("pause");
+        system("cls");
+        return main();
+    }
+    else if (pilih == 0)
+    {
+        return 0;
     }
 }
+
+// int main()
+// {
+//     int pilih;
+//     do
+//     {
+//         Print();
+//         cout << "=========================Menu========================" << endl;
+//         cout << "1. Tambah Mahasiswa" << endl;
+//         cout << "2. Tampilkan Mahasiswa" << endl;
+//         cout << "3. Urutkan Mahasiswa Berdasarkan NIM" << endl;
+//         cout << "4. Keluar" << endl;
+//         cout << "Pilih menu";
+//         cin >> pilih;
+//         cout << "================================================================\n";
+//         switch (pilih)
+//         {
+//         case 1:
+//             tambah();
+//             getch();
+//             break;
+//         case 2:
+//             tampil();
+//             getch();
+//             break;
+//         case 3:
+//             urut();
+//             getch();
+//             break;
+//         case 4:
+//             exit(0);
+//             getch();
+//             break;
+//         default:
+//             cout << "Pilihan yang anda masukan salah" << endl;
+//             break;
+//         }
+//         system("cls");
+//     } while (pilih != 4);
+// }
